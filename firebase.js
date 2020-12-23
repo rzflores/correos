0717084bae8130eea7ce865e46c102ac2d749b04
rflores <repaflocha@hotmail.com>
2020-12-23 11:41:01 -0500

    primer commit

---
+++ firebase.js
@@ -0,0 +1,11 @@
+   var firebaseConfig = {
+                apiKey: "AIzaSyA2tZ-WAl4g0SOn_seMOOiO1RHIYESux8s",
+                authDomain: "bdcorreos-2747f.firebaseapp.com",
+                projectId: "bdcorreos-2747f",
+                storageBucket: "bdcorreos-2747f.appspot.com",
+                messagingSenderId: "443743439478",
+                appId: "1:443743439478:web:5931aaad5b8bf6bbf5ed57",
+                measurementId: "G-6T5DEFKJ58"
+            };
+            // Initialize Firebase
+            firebase.initializeApp(firebaseConfig);
---
+++ subcripcion.js
@@ -0,0 +1,45 @@
+var db = firebase.database();
+
+
+function FechaHoy(){
+    var fh = new Date();
+    return fh.getFullYear()+"-"+(fh.getMonth()+1)+"-"+fh.getDate()+" "+fh.getHours()+":"+fh.getMinutes();
+}
+
+
+
+ 
+
+
+function GuardarCorreo(email){
+    
+    db.ref('correos/').push({
+        correo:email,
+        date:FechaHoy()
+     })
+
+     ImprimirListaCorreos();
+}
+function ImprimirListaCorreos(){
+    let listacorreos = document.querySelector("#listaCorreos");            
+    let reference = firebase.database().ref('correos/');    
+    listacorreos.innerHTML  = "";
+    reference.on('child_added',(snapshot) =>{
+        let dataCorreos = snapshot.val();       
+        listacorreos.innerHTML  += `<li>${dataCorreos.correo}</li>`        
+      });
+    
+}
+document.addEventListener("DOMContentLoaded", function(event) {
+    ImprimirListaCorreos();
+  });
+
+
+
+ 
+document.querySelector("#EnviarCorreo").addEventListener('click',()=>{
+    let email = document.querySelector("#email").value;            
+    
+    GuardarCorreo(email);
+  
+})
